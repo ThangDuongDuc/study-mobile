ea1f7edb0518157ee11c15af4f240b74c480f695
AnthonyElian <thangduc.duong14@gmail.com>
2022-04-23 22:39:01 +0700

    Content Provider - read/write contacts - errors when delete

---
+++ ReadContacts_ContentProvider/app/src/main/java/com/ddt/readcontacts_contentprovider/MainActivity.java
@@ -0,0 +1,43 @@
+package com.ddt.readcontacts_contentprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ddt.readcontacts_contentprovider.activities.ContactsActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.contacts:
+                Intent i = new Intent(getApplicationContext(), ContactsActivity.class);
+                startActivity(i);
+                // do your code
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
